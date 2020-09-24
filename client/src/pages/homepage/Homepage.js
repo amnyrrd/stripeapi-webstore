0f2c44f274a59a459bbea6694c011ce00e1b8db4
amnyrrd <amnyrrd@gmail.com>
2020-09-23 18:04:42 -0700

    addedserviceworker, https, updated manifest for pwa

--- client/src/pages/homepage/Homepage.js
+++ client/src/pages/homepage/Homepage.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { Profiler } from 'react';
 import Directory from '../../components/directory/Directory';
 import { HomepageContainer } from './Homepage.styles';
 
 const Homepage = () => {
   return (
     <HomepageContainer>
+    <Profiler id='Directory' onRender={(id, phase, actualDuration) => {
+      console.log({
+        id, 
+        phase,
+        actualDuration
+      })
+    }}>
       <Directory />
+      </Profiler>
     </HomepageContainer>
   );
 };
